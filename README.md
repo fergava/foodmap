515837c88d242c56720e3fed65334f517e084ea8
Fernanda Gava <nanda_gava@hotmail.com>
2018-11-04 17:54:28 -0200

    atualizado templates

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # foodmap
+
+Aplicação mobile-first que busca restaurantes próximos através da API GoogleMaps .
+
+### Demonstração:
+
+![foodmap](https://raw.githubusercontent.com/fergava/foodmap/master/foodmap.bmp)
+
+## Funcionalidades:
+
+1. Tela splash com duração de 3 segundos que redireciona para a Home;
+
+2. Home com opção de todos os restaurantes próximos (Os restaurantes sugeridos para este exercício foram acessados por meio de um banco de dados interno com 9 opções);
+
+3. Localização dos restaurantes via GoogleMaps;
+
+4. Após seleção do filtro é exibido apenas os restaurantes correspondentes;
+
+5. Selecionando a imagem do resturante é exibido a informação dele por um modal;
+
+6. Fechando o modal, voltará a tela anterior.
+
+## Roadmap oficial do projeto
+
+### versão 3.0.0 (sem previsão)
+- acesso a um banco de dados com mais opções de restaurantes;
+- mudar API de mapas.
+
+### versão 2.0.0 (Novembro/2018)
+- refatorado com uso de ES6.
+
+### versão 1.0.0 (Agosto/2018)
+- funcionalidades já citadas.
+
+## Tecnologias utilizadas:
+
+- HTML5
+- CSS3
+- JQuery
+- Bootstrap 4
+- ES6
+- Mobile First
+- [Maps JavaScript API](https://developers.google.com/maps/documentation/javascript/tutorial).
--- js/app.js
+++ js/app.js
@@ -1,97 +1,132 @@
-$(document).ready(function () {
-    
-  // splash
-  $("#splash").delay("3000").slideUp("slow");
-  $("#main-page").delay("3000").fadeIn("slow");
-  
-  // criar seções
-  $("#main-page").append("<section id='all-photos' class='w-75 d-flex flex-wrap justify-content-center align-items-center'></section>");
-
-  $("#main-page").append("<section id='section-modal'></section>");
-  
-  // carregar todas as imagens na pag principal
-  function showAllPhotos() {
-    restaurantes.map((restaurant, index) => {
-      $("#all-photos").append($("<div> <img id='img" + index + "' class='size-thumbnail rounded m-1' data-toggle='modal' data-target='#start-modal"+ index + "' alt='" + restaurant.name + "' src='" + restaurant.image + "'> </div>"));
-      $("#img" + index).click(createModal(restaurant, index));
-    });
-  };
+$(document).ready(() => {
+  splash()
   showAllPhotos();
-
-  // autocomplete
-  function getRestaurantsOfData() {
-    var arrRestaurants = [];
-      
-    restaurantes.map((restaurant) => {
-      arrRestaurants.push(restaurant.name)
-      arrRestaurants.push(restaurant.type)
-    });
-
-    arrRestaurants = arrRestaurants.filter(function (x, y) {
-      return arrRestaurants.indexOf(x) === y;
-
-    });
-
-    $("#search").autocomplete({
-      source: arrRestaurants
-    });
-  }
-  getRestaurantsOfData()
-
-  // mostrar fotos filtradas
-  function showFilterPhotos(restaurant, index) {
-    $("#all-photos").append($("<div class='restaurant-modal text-center'> <p class='p-0 m-0'>" + restaurant.name + "</p> <img id='img" + index + "' class='size-thumbnail-2 rounded m-1' data-toggle='modal' data-target='#start-modal"+ index +"' alt=" + restaurant.name + " src=" + restaurant.image + "></div>"));
-    
-    $("#img" + index).click(createModal(restaurant, index));
-  }
-
-  // criar o modal
-  function createModal(restaurant, index) {
-    $("#section-modal").append("<div id='start-modal" + index + "' class=' modal' tabindex='-1' role='dialog'><div class='modal-dialog-centered' role='document'>        <div class='modal-content text-center'><div class='modal-header'> <h3 class='text-uppercase font-weight-bold'>" + restaurant.type + "</h3> <button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div><div class='modal-body'><div id='" + index + "' class='restaurant-modal border rounded mb-3 p-2 d-flex flex-column align-items-center'> <h3 class='font-weight-bold'>" + restaurant.name + "</h3> <img id='img" + index + "' class='size-thumbnail-2 rounded m-1' data-toggle='modal' data-target='#start-modal" + index + "' alt=" + restaurant.name + " src=" + restaurant.image + "> <p class='text-dark mb-2'>" + restaurant.description + "</p> <button type='button' class='btn btn-warning'>Pedir Agora!</button></div>       </div></div></div></div>");
-  }
-
-  // pegar o valor do input e filtrar quando click do botão
-  $("#search-button").click( function searchRestaurants(event) {
-    event.preventDefault();
-    
-    $("#all-photos").empty();
-    var resultSearch = $("#search").val();
-    
-    restaurantes.forEach( (restaurant, index) => {
-      if (resultSearch === restaurant.name || resultSearch === restaurant.type) {
-        showFilterPhotos(restaurant, index);
-        myMap();
-      };
-    });
-    resultSearch = ($("#search").val("")); 
-  });
-
+  restaurantsAutoComplete()
+  handleClick()
 });
 
+function splash() {
+  $("#splash").delay("3000").slideUp("slow");
+  $("#main-page").delay("3000").fadeIn("slow");
+}
+// autocomplete
+function restaurantsAutoComplete() {
+  let arrRestaurants = [];
+  restaurantes.map((restaurant) => {
+    arrRestaurants.push(restaurant.name)
+    arrRestaurants.push(restaurant.type)
+  });
+  arrRestaurants = arrRestaurants.filter((x, y) => {
+    return arrRestaurants.indexOf(x) === y;
+  });
+  $("#search").autocomplete({
+    source: arrRestaurants
+  });
+}
+function handleClick() {
+  $("#search-button").click(searchRestaurants);
+}
+function searchRestaurants(event) {
+  event.preventDefault();
+  let resultSearch = $("#search").val();
+  if (resultSearch === "") {
+    alert("Busque algum restaurante ou cozinha!");
+  } else {
+    $("#photos").empty();
+    showFilterPhotos()
+  }
+  resultSearch = $("#search").val("");
+}
+// carregar todas imagens na pag principal
+function showAllPhotos() {
+  restaurantes.forEach((restaurant, index) => {
+    renderPhotos(restaurant, index)
+  });
+};
+// carregar apenas imagens correspondentes ao filtro
+function showFilterPhotos() {
+  let resultSearch = $("#search").val();
+  restaurantes.filter((restaurant, index) => {
+    if (resultSearch === restaurant.name || resultSearch === restaurant.type) {
+      renderPhotos(restaurant, index);           
+    };
+  });
+}
+// mostrar imagens
+function renderPhotos(restaurant, index) {
+  $("#photos").append(`
+      <div>
+        <img  id=${"img" + index} 
+              class="size-thumbnail rounded m-1" 
+              data-toggle="modal" 
+              data-target=${"#start-modal" + index}
+              alt=${restaurant.name} 
+              src=${restaurant.image}
+        /> 
+        </div>
+      `
+  );
+  $("#img" + index).click(createModal(restaurant, index));
+}
+// criar o modal
+function createModal(restaurant, index) {
+  $("#section-modal").append(`
+      <div  id=${"start-modal" + index} class="modal" tabindex="-1" role="dialog">
+        <div  class="modal-dialog-centered" role="document">        
+          <div class="modal-content text-center">
+            <div class="modal-header"> 
+              <h3 class="text-uppercase font-weight-bold">
+                ${restaurant.type}
+              </h3> 
+              <button type="button" class="close"data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">
+                &times;
+              </span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <div id=${index} class="restaurant-modal border rounded mb-3 p-2 d-flex flex-column align-items-center">
+                <h3 class="font-weight-bold">
+                ${restaurant.name}
+                </h3>
+                <img id=${"img" + index} class="size-thumbnail-2 rounded m-1" data-toggle="modal" data-target=${"#start-modal" + index} alt=${restaurant.name} src=${restaurant.image}> 
+                <p class="text-dark mb-2">
+                  ${restaurant.description}
+                </p> 
+                <button type="button" class="btn btn-warning">
+                  Pedir Agora!
+                </button>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+  `);
+}
 // GOOGLE MAPS API
 function myMap() {
-  
-  var map = new google.maps.Map(
-    document.getElementById('google-map'), {
-      zoom: 15, center: {
-        lat:-23.557567, lng: -46.658615
+  const map = new google.maps.Map(
+    document.getElementById("google-map"), {
+      zoom: 15, 
+      center: {
+        lat: -23.557567, 
+        lng: -46.658615
       }
-    });
-  
-  for (var i of restaurantes) {
-    var coords = [];
+  });
+  restaurantes.map((i) => {
+    let coords = [];
     coords.push(i.latitude, i.longitude)
-    var address = { lat: coords[0], lng: coords[1] }
-    var marker = new google.maps.Marker({
+    let address = { lat: coords[0], lng: coords[1] }
+    let marker = new google.maps.Marker({
       position: address,
       animation: google.maps.Animation.DROP,
       map: map
     });
-    marker.addListener('click', function () {
+    $(marker).click(() => {
       const infowindow = new google.maps.InfoWindow({
         content: i.name
       });
       infowindow.open(map, marker);
     });
-  }
+  })  
 }
